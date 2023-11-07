eed7a5e7cb44a867f65830b1ca6f3101345fe3a5
Kevin Salinas <kevin.salinas04@utrgv.edu>
2023-11-07 13:48:51 -0600

    Updated Files

---
+++ app.py
@@ -0,0 +1,30 @@
+from flask import Flask, render_template, request
+
+app = Flask(__name__)
+filepath = "C:/Users/KevSa/source/repos/Python/WK 10/static/aboutMe.txt"
+
+# Function to read in details for page
+def readDetails(filepath):
+    with open(filepath, 'r') as f:
+        return [line for line in f]
+
+# Make a homepage
+@app.route('/')
+def homepage():
+    aboutMe = readDetails(filepath)
+    return render_template("homepage.html", name = 'Kevin', aboutMe = aboutMe)
+
+@app.route('/hello/<name>')
+def hello(name):
+    listOfNames = [name, "Yoyo", "Yennifer"]
+    return render_template('name.html', Sname = name, nameList = listOfNames)
+
+@app.route('/form', methods=['GET', 'POST'])
+def formDemo(name=None):
+    if request.method == 'POST':
+        name=request.form['name']
+    return render_template('form.html', name=name)
+
+# Add the option to run this file directly
+if __name__ == "__main__":
+    app.run(debug=True)
